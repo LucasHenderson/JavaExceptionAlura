190567a2f27e38243e970ec8094aef04a0caed58
LucasHenderson <80459555+LucasHenderson@users.noreply.github.com>
2022-09-15 16:37:40 -0300

    Add files via upload

--- java-pilha/src/Fluxo.java
+++ java-pilha/src/Fluxo.java
@@ -1,31 +1,32 @@
 
 public class Fluxo {
 	public static void main(String[] args) {
-        System.out.println("Inicia do main");
-        metodo1();
-        System.out.println("Fim do main");
-    }
+		System.out.println("Inicia do main");
+		try {
+			metodo1();
+		} catch (ArithmeticException | NumberFormatException | MinhaException e) {
+			String msg = e.getMessage(); //mostra oq causou a exception
+        	System.out.println("Exception foi capturada: " + msg);
+		}
+		System.out.println("Fim do main");
+	}
 
-    private static void metodo1() {
-        System.out.println("Inicia do metodo1");
-        metodo2();
-        System.out.println("Fim do metodo1");
-    }
+	private static void metodo1() {
+		System.out.println("Inicia do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
 
-    private static void metodo2() {
-        System.out.println("Inicia do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-            try {
-            	int a = i/0;
-            } catch (ArithmeticException | NumberFormatException e) {
-            	String msg = e.getMessage(); //mostra oq causou a exception
-            	System.out.println("Exception foi capturada: " + msg);
-            	e.printStackTrace(); //mostra o rastro de onde passou a exception
-            } catch (NullPointerException e) {
-            	System.out.println("Caiu nessa exception");
-            }
-        }
-        System.out.println("Fim do metodo2");
-    }
+	private static void metodo2() {
+		System.out.println("Inicia do metodo2");
+		
+		//ArithmeticException exception = new ArithmeticException("deu ruim");
+		//throw exception;
+		
+		//throw new NullPointerException(); //geralmente vai ser feito assim, sem criar uma referencia
+		
+		throw new MinhaException("deu ruim, minha exception");
+		
+		//System.out.println("Fim do metodo2");
+	}	
 }
---
+++ java-pilha/src/FluxoComTratamento.java
@@ -0,0 +1,31 @@
+
+public class FluxoComTratamento {
+	public static void main(String[] args) {
+        System.out.println("Inicia do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicia do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicia do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            try {
+            	int a = i/0;
+            } catch (ArithmeticException | NumberFormatException e) {
+            	String msg = e.getMessage(); //mostra oq causou a exception
+            	System.out.println("Exception foi capturada: " + msg);
+            	e.printStackTrace(); //mostra o rastro de onde passou a exception
+            } catch (NullPointerException e) {
+            	System.out.println("Caiu nessa exception");
+            }
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
